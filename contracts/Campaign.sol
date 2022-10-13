091679a6e75451cfa85391c5b91d7ed8764b989c
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-13 19:40:21 +0530

    add

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -3,9 +3,73 @@ pragma solidity 0.4.17;
 contract Campaign {
     address public manager;
     uint minimumContribution;
+    address[] public approvers;
     
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
