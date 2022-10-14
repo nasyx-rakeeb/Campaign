0a284c9f11595460a7a3ff05e89eba4022e2cf05
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-14 18:46:49 +0530

    add

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,6 +1,13 @@
 pragma solidity 0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint minimumContribution;
     address[] public approvers;
