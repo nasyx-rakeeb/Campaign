fd82cb5d0b8e170ddd9988ea498aa04f5312a986
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-14 18:57:06 +0530

    add

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -27,6 +27,16 @@ contract Campaign {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
     }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+        requests.push(newRequest);
+    }
 }
 
 
