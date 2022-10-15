5e9428d3984cc3df75f7530a1d0059082ffa57fa
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-15 12:22:53 +0530

    add

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,9 +2,14 @@ pragma solidity 0.4.17;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
+    
     function createCampaign(uint minimum) public {
         address newCapaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCapaign);
+    }
+    
+    function getDeployedCampaigns() public view returns(address[]) {
+        return deployedCampaigns;
     }
 }
 
